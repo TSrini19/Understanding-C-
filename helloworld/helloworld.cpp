7bf313fad08936be13a6d3dae3dbb9aa01a93b55
Taarun <tsrini.roboticist@gmail.com>
2022-04-22 19:34:14 +0530

    helloworld

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,12 @@
+// We start the hello world program by including the header files
+#include <iostream>
+
+// we define the string "Hello World!!!" inside the main function
+int main()
+{
+  // We use the cout and endl functionalities from the std class which is imported from <iostream>
+  std::cout << "Hello World!!!" << std::endl;
+
+  return 0;
+}
+

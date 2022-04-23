fe3a01c18456ba5cb7e52009ca9169324a24cbde
Taarun <tsrini.roboticist@gmail.com>
2022-04-23 20:49:17 +0530

    data modifiers

---
+++ 02_datatype_modifier/modifier.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+// There are four main datatype modifiers: short, long, signed, and unsigned
+ std::cout << "size of int " << sizeof(int) << " bytes " << std::endl;
+ std::cout << "size of short int " << sizeof(short int) << " bytes " << std::endl;
+ std::cout << "size of long int " << sizeof(long int) << " bytes " << std::endl;
+ std::cout << "size of signed long int " << sizeof(signed long int) << " bytes " << std::endl;
+ std::cout << "size of unsigned long int " << sizeof(unsigned long int) << " bytes " << std::endl;
+  return 0;
+ }
+ 

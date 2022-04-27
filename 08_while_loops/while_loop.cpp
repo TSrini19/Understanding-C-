54251702833a5b2acd2da13658d1a28c15b039e8
Taarun <tsrini.roboticist@gmail.com>
2022-04-27 20:50:20 +0530

    while loops

--- 08_while_loops/while_loop.cpp
+++ 08_while_loops/while_loop.cpp
@@ -7,6 +7,7 @@ int main()
  std::cout << "Enter a value for a " << std::endl;
  std::cin >> a;
  
+ //the while loop consist only the condition. If the condition is true it will execute statements inside the loop, else exit the loop.
  while(a < 10)
  { 
  std::cout << "Value of a is less than 10" << std::endl;
